146d0ed562c52213322b89ba6fb8290e666ff57a
Akki283 <akshaygaikwad283@gmail.com>
2023-05-10 21:31:13 +0530

    day1

---
+++ Day1.py
@@ -0,0 +1,39 @@
+
+# reverse the string
+
+# ----using for loop
+
+# st="manish"
+# # rest=""
+# for i in range(len(st)-1,-1,-1):
+#     rest+=st[i]
+# print(rest)
+
+# using built_in
+
+# string='man'
+# restring=string[::-1]
+# print(restring)
+
+# ----using list
+
+# string='man'
+# lst=list(string)
+# lst.reverse()
+# new=''.join(lst)
+# print(new)
+
+
+# -------using while loop
+
+# st='manish'
+# rst=''
+# i=len(st)-1
+# while i>=0:
+#     rst=rst+st[i]
+#     i=i-1
+# print(rst)
+
+
+# -----------
+
